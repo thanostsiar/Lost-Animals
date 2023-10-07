1cb68e1aa8cdf47ed817fc5d699977cb86dc9597
Thanasis Tsiaras <thanostsiar@gmail.com>
2023-10-07 19:57:18 +0300

    Initial Commit

---
+++ src/main/java/com/lostanimals/lostanimalsbackend/LostAnimalsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lostanimals.lostanimalsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAnimalsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAnimalsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lostanimals/lostanimalsbackend/LostAnimalsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lostanimals.lostanimalsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAnimalsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
