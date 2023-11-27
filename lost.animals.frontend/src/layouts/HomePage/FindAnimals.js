775070cdd982eb7ec766035d2dbcaa9c0a6871f9
Thanasis Tsiaras <thanostsiar@gmail.com>
2023-11-27 02:45:17 +0200

    Added initial layouts

---
+++ lost.animals.frontend/src/layouts/HomePage/FindAnimals.js
@@ -0,0 +1,16 @@
+export const FindAnimals = () => {
+    return (
+        <div className='p-5 mb-4 header'>
+            <div className='container-fluid py-5 text-white
+                d-flex justify-content-center align-items-center'>
+                <div>
+                    <h1 className='display-5 fw-bold'>Find lost animals</h1>
+                    <p className='col-md-8 fs-4'>Create alerts if you lost or found a pet </p>
+                    <a type='button' className='btn btn-main-color btn-lg text-white' href='#'>
+                        Create Alert
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
