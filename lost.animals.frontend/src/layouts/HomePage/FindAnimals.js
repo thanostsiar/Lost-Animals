00e1942ad921c3ec4104b83c0daca220e37c81c8
Thanasis Tsiaras <thanostsiar@gmail.com>
2023-12-03 16:40:11 +0200

    Added a carousel component to the home page

--- lost.animals.frontend/src/layouts/HomePage/FindAnimals.js
+++ lost.animals.frontend/src/layouts/HomePage/FindAnimals.js
@@ -6,11 +6,11 @@ export const FindAnimals = () => {
                 <div>
                     <h1 className='display-5 fw-bold'>Find lost animals</h1>
                     <p className='col-md-8 fs-4'>Create alerts if you lost or found a pet </p>
-                    <a type='button' className='btn btn-main-color btn-lg text-white' href='#'>
+                    <a type='button' className='btn main-color btn-lg text-white' href='#'>
                         Create Alert
                     </a>
                 </div>
             </div>
         </div>
     );
-};
+}; 
