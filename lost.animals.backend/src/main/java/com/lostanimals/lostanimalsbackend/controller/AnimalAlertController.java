7902bd84779c8905e222729abbc4c7d936985e51
Thanasis Tsiaras <thanostsiar@gmail.com>
2023-12-31 22:20:23 +0200

    Implemented the backend for the Search Animal Page

--- lost.animals.backend/src/main/java/com/lostanimals/lostanimalsbackend/controller/AnimalAlertController.java
+++ lost.animals.backend/src/main/java/com/lostanimals/lostanimalsbackend/controller/AnimalAlertController.java
@@ -1,9 +1,43 @@
 package com.lostanimals.lostanimalsbackend.controller;
 
+import com.lostanimals.lostanimalsbackend.entity.AnimalAlert;
+import com.lostanimals.lostanimalsbackend.service.AnimalAlertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/animal-alerts")
 public class AnimalAlertController {
+
+    private final AnimalAlertService animalAlertService;
+
+    @Autowired
+    public AnimalAlertController(AnimalAlertService animalAlertService) {
+        this.animalAlertService = animalAlertService;
+    }
+
+    @GetMapping("/search/findAll")
+    public ResponseEntity<List<AnimalAlert>> getAllAnimalAlerts() {
+        List<AnimalAlert> animalAlerts = animalAlertService.getAllAnimalAlerts();
+        return ResponseEntity.ok(animalAlerts);
+    }
+    
+    @GetMapping("/search/findByTitle")
+    public ResponseEntity<List<AnimalAlert>> findAnimalAlertByTitle(String title) {
+        List<AnimalAlert> alerts = animalAlertService.findAnimalAlertByTitle(title);
+        return ResponseEntity.ok(alerts);
+    }
+
+    @GetMapping("/search/findBySpecies")
+    public ResponseEntity<List<AnimalAlert>> findAnimalAlertByAnimalSpecies(String species) {
+        List<AnimalAlert> alerts = animalAlertService.findAnimalAlertByAnimalSpecies(species);
+        return ResponseEntity.ok(alerts);
+    }
 }
--- lost.animals.backend/src/main/java/com/lostanimals/lostanimalsbackend/entity/Animal.java
+++ lost.animals.backend/src/main/java/com/lostanimals/lostanimalsbackend/entity/Animal.java
@@ -1,5 +1,6 @@
 package com.lostanimals.lostanimalsbackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
@@ -19,12 +20,64 @@ public class Animal {
     @Column(name = "SPECIES", nullable = false)
     private String species;
 
-    @Column(name = "BREED", nullable = false)
-    private String breed;
-
-    @Column(name = "COLOR", nullable = true)
+    @Column(name = "COLOR", nullable = false)
     private String color;
 
     @OneToMany(mappedBy = "animal", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonBackReference
     private Set<AnimalAlert> animalAlertSet = new HashSet<>();
+
+    public Animal() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Set<AnimalAlert> getAnimalAlertSet() {
+        return animalAlertSet;
+    }
+
+    public void setAnimalAlertSet(Set<AnimalAlert> animalAlertSet) {
+        this.animalAlertSet = animalAlertSet;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", species='" + species + '\'' +
+                ", color='" + color + '\'' +
+                ", animalAlertSet=" + animalAlertSet +
+                '}';
+    }
 }
