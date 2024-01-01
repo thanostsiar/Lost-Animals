6200ca129640dd1bb7773079654e4cdbad09a2a5
Thanasis Tsiaras <thanostsiar@gmail.com>
2024-01-01 21:33:34 +0200

    Implemented routing

--- lost.animals.frontend/src/App.tsx
+++ lost.animals.frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
@@ -8,11 +8,22 @@ import { SearchAnimalsPage } from './layouts/SearchAnimalsPage/SearchAnimalsPage
 
 export const App = () => {
   return (
-    <div>
-      <Navbar/>
-      {/* <HomePage/> */}
-      <SearchAnimalsPage/>
-      <Footer/>
+    <div className='d-flex flex-column min-vh-100'>
+      <Navbar />
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/home'>
+            <HomePage />
+          </Route>
+          <Route path='/search'>
+            <SearchAnimalsPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 };
--- lost.animals.frontend/src/layouts/NavbarAndFooter/Footer.tsx
+++ lost.animals.frontend/src/layouts/NavbarAndFooter/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
     return (
@@ -8,9 +9,14 @@ export const Footer = () => {
                 <p className='col-md-4 mb-0 text-white'>© Lost Animals, Inc</p>
                 <ul className='nav navbar-dark col-md-4 justify-content-end'>
                     <li className='nav-item'>
-                        <a href='#' className='nav-link px-2 text-white'>
+                        <Link to='/home' className='nav-link px-2 text-white'>
                             Home
-                        </a>
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/search' className='nav-link px-2 text-white'>
+                            Search Animals
+                        </Link>
                     </li>
                 </ul>
             </footer>
